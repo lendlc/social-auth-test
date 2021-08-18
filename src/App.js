d7d9d8147cb44ac6b614c766bc6694cdc86dcaab
lendlc <johnlendl.cuyugan20@gmail.com>
2021-08-18 23:56:03 +0800

    Pro moves

--- src/App.js
+++ src/App.js
@@ -1,22 +1,49 @@
-import logo from './logo.svg';
 import './App.css';
+import { GoogleLogin } from 'react-google-login';
+import FacebookLogin from 'react-facebook-login';
+
+//Client ID for Google/Fb
+import secret from './config/secret.json'
+import axios from 'axios'
 
 function App() {
+
+
+  const googleResponse = async (res) => {
+    console.log(res)
+    try {
+      const response= await axios.post('http://127.0.0.1:8000/api/v1/auth/signup/google/', {"id_token": res.tokenId})
+      console.log(response)
+      console.log(response.data.key , response.data.user )
+    } catch (error) {
+      console.log(error.response.data)
+    }
+    
+  }
+
+  const facebookResponse = async (res) => {
+    console.log(res)
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
+        <GoogleLogin
+          clientId={secret.GOOGLE_CLIENT_ID}
+          buttonText="Login"
+          onSuccess={googleResponse}
+          onFailure={googleResponse}
+          cookiePolicy={'single_host_origin'}
+        />
+
+        <FacebookLogin
+          appId={4375200082540049}
+          autoLoad={true}
+          fields="name,email,picture"
+          onClick={() => alert('Asd')}
+          callback={facebookResponse} 
+        />
       </header>
     </div>
   );
